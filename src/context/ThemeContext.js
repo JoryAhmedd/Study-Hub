0d17b0c2313720af44ce7ae625b1dfbdab2b2fae
Joury Ahmed <joryahmedd590@gmail.com>
2026-09-11 13:44:45 -0700

    Dark Mode

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,21 +1,43 @@
 "use client";
 
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 
 const ThemeContext = createContext();
 
-export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("light");
+const initialState = {
+  theme: "light",
+};
 
-  const toggleTheme = () =>
-    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+function reducer(state, action) {
+    const {type} = action;
+
+    switch (type) {
+        case "TOGGLE_THEME": 
+            return {
+
+                ...state,
+                theme: state.theme === "light" ? "dark" : "light",
+            }
+
+        default:
+            return state;
+        
+    }
+}
+
+export function ThemeProvider({ children }) {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+  const toggleTheme = () => {
+    dispatch({ type: "TOGGLE_THEME" });
+  };
 
   useEffect(() => {
-    document.documentElement.classList.toggle("dark", theme === "dark");
-  }, [theme]);
+    document.documentElement.classList.toggle("dark", state.theme === "dark");
+  }, [state.theme]);
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider value={{ theme: state.theme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
